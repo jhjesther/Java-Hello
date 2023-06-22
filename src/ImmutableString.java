2bcbdf2360277337721f182654ed1e1c7b9a1a35
DESKTOP-A6P16AB\USER <jhjesther@gmail.com>
2023-06-22 17:47:50 +0900

    fourth commit

--- src/ImmutableString.java
+++ src/ImmutableString.java
@@ -21,9 +21,13 @@ public class ImmutableString {
 			System.out.println("str1과str2는 다른 인스턴스 참조");
 		
 		// 참조 변수의 참조 값 비교
-		if (str3 == str4)
+
+		if (str3 == str4)//다른 언어에서는.
+			
 			System.out.println("str3과 str4는 동일 인스턴스 참조");
+		
 		else
+			
 			System.out.println("str3과 str4는 다른 인스턴스 참조");
 	}
 }
