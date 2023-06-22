07ceeae64c11dc5b6e1cea6abb27f2b708accaee
DESKTOP-A6P16AB\USER <jhjesther@gmail.com>
2023-06-22 17:47:01 +0900

    fourth commit

--- src/ImmutableString.java
+++ src/ImmutableString.java
@@ -2,17 +2,24 @@
 public class ImmutableString {
 
 	public static void main(String[] args) {
+		
 		String str1 = "Simple String";
+		
 		String str2 = "Simple String";
 
 		String str3 = new String("Simple String");
+		
 		String str4 = new String("Simple String");
+		
 		// 참조변수의 참조 값 비교
-
-		if (str1 == str2)
+		
+		if (str1.equals(str3))
+			
 			System.out.println("str1과 str2는 동일 인스턴스 참조");
+		
 		else
 			System.out.println("str1과str2는 다른 인스턴스 참조");
+		
 		// 참조 변수의 참조 값 비교
 		if (str3 == str4)
 			System.out.println("str3과 str4는 동일 인스턴스 참조");
