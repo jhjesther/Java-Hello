cbd7a91bc0659617ffd2af46b7a3429da2304e2e
DESKTOP-A6P16AB\USER <jhjesther@gmail.com>
2023-06-23 18:19:35 +0900

    fifth commit

--- src/RectangleArray.java
+++ src/RectangleArray.java
@@ -1,15 +1,30 @@
 
 public class RectangleArray {
+	
 	public static void main(String[] args){
     
 	Rectangle3[]arrRec= new Rectangle3[2];
-	}
+	
+	
 
-	arrRec[0] = new Rectangle1(40.50);
-	arrRec[1] = new Rectangle1(20,50);
+	arrRec[0] = new Rectangle3(40.50);
+
+	arrRec[1] = new Rectangle3(20,50);
 	
 	double areaRec = 0;
 	
-	for(int i=0; i < arrRectangle.length;i++) {
+	for(int i=0; i < arrRec.length;i++) {
+	   
+	         int width, height;
+	         Scanner sc = new Scanner(System.in);
+	         width = sc.nextInt();
+	         height = sc.nextInt();         
+	      
+	         arrRec[i] = new Rectangle3(width,height);
+	      }
+		
 		areaRec = areaRec + arrRec[i].getArea();}
-	} 	System.out.println(areaRec);
+	} 	
+
+System.out.println(areaRec);
+}
