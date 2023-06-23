17ed453b338e85c8ff225bb1a8c94dbc9ed1ffc7
DESKTOP-A6P16AB\USER <jhjesther@gmail.com>
2023-06-23 18:29:30 +0900

    fifth commit

--- src/Rectangle3.java
+++ src/Rectangle3.java
@@ -1,4 +1,17 @@
+class Rectangle3 {
 
-public class Rectangle3 {
+   private double width, height; // 변수선언 > 게터세터 넣기
+
+   public Rectangle3() {
+   }
+
+   public Rectangle3(double width, double height) {
+      this.width = width;
+      this.height = height;
+   }
+
+   public double getArea() {
+      return width * height;
+   }
 
 }
--- src/RectangleArray.java
+++ src/RectangleArray.java
@@ -1,30 +1,41 @@
+import java.util.Scanner;
 
 public class RectangleArray {
 	
 	public static void main(String[] args){
     
-	Rectangle3[]arrRec= new Rectangle3[2];
+	Rectangle3[] arrRec= new Rectangle3[2];
 	
 	
 
-	arrRec[0] = new Rectangle3(40.50);
+	arrRec[0] = new Rectangle3(40,50);
 
 	arrRec[1] = new Rectangle3(20,50);
 	
-	double areaRec = 0;
+
 	
 	for(int i=0; i < arrRec.length;i++) {
 	   
 	         int width, height;
 	         Scanner sc = new Scanner(System.in);
+	         
+	         System.out.print("가로:");
+	         
 	         width = sc.nextInt();
+	         System.out.print("세로:");
+	         
 	         height = sc.nextInt();         
 	      
 	         arrRec[i] = new Rectangle3(width,height);
-	      }
-		
-		areaRec = areaRec + arrRec[i].getArea();}
-	} 	
+	         
+	}
+	
+	double areaRec = 0;
+	for(int i=0; i < arrRec.length;i++) {
+		areaRec = areaRec + arrRec[i].getArea();
+	}
+	
+	System.out.println(areaRec);
 
-System.out.println(areaRec);
+	}
 }
