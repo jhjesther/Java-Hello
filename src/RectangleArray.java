34c531703507e678b47f850736988297a08cabb1
DESKTOP-A6P16AB\USER <jhjesther@gmail.com>
2023-06-23 18:08:09 +0900

    fourth commit

---
+++ src/RectangleArray.java
@@ -0,0 +1,15 @@
+
+public class RectangleArray {
+	public static void main(String[] args){
+    
+	Rectangle3[]arrRec= new Rectangle3[2];
+	}
+
+	arrRec[0] = new Rectangle1(40.50);
+	arrRec[1] = new Rectangle1(20,50);
+	
+	double areaRec = 0;
+	
+	for(int i=0; i < arrRectangle.length;i++) {
+		areaRec = areaRec + arrRec[i].getArea();}
+	} 	System.out.println(areaRec);
--- src/whileLoop4.java
+++ src/whileLoop4.java
@@ -1,4 +1,39 @@
 
 public class whileLoop4 {
+	 Scanner sc = null;
+     Grade2 grade = null;
+     int kor, eng, math;
+     String name;
+     ///////////////////////////////////
+           
+     while (true) {
+        sc = new Scanner(System.in);         
 
+        System.out.print("이름:");
+        name = sc.next();
+        
+        System.out.print("국어:");
+        kor = sc.nextInt();
+
+        System.out.print("영어:");
+        eng = sc.nextInt();
+        
+        System.out.print("수학:");
+        math = sc.nextInt();
+        
+        grade = new Grade2(name, kor, eng, math);
+
+        grade.showInfo();
+        
+
+        System.out.print("계속?");
+        String choice = sc.next();
+
+        if (choice.compareToIgnoreCase("yes") == 0 || choice.compareToIgnoreCase("y") == 0)
+           continue;
+        else
+           break;
+     }
+
+     System.out.println("종료되었습니다.");
 }
