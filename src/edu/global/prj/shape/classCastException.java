914802a5830f0af7c4281cea78a7dc65d5d47882
DESKTOP-A6P16AB\USER <jhjesther@gmail.com>
2023-06-29 15:51:01 +0900

    twelfth commit

---
+++ src/edu/global/prj/shape/classCastException.java
@@ -0,0 +1,19 @@
+package edu.global.prj.shape;
+
+public class classCastException {
+class Board{}
+class PBoard extends Board{}
+
+class classCast{
+	public static void main(String[] args) {
+		Board pbd1 = new PBoard();
+		PBoard pbd2 = (PBoard)pbd1;
+		
+		System.out.println("..intermediate location..");
+		Board ebd1 = new Board();
+		PBoard ebd2 = (PBoard)ebd1;
+		
+	}
+}
+
+}
